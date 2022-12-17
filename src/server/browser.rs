d6a2b6bdf6b1e517036462d81ce5594606c4aa5a
John Doty <john@d0ty.me>
2022-12-17 08:19:56 -0800

    Fix non-unix build

--- src/server/browser.rs
+++ src/server/browser.rs
@@ -3,9 +3,17 @@ use anyhow::Result;
 use tokio::sync::mpsc;
 
 #[cfg(target_family = "unix")]
+mod browser_unix;
+
+#[cfg(target_family = "unix")]
 use browser_unix::handle_browser_open_impl;
 
-mod browser_unix;
+#[cfg(not(target_family = "unix"))]
+async fn handle_browser_open_impl(
+    messages: mpsc::Sender<Message>,
+) -> Result<()> {
+    std::future::pending().await
+}
 
 #[inline]
 pub async fn handle_browser_open(
